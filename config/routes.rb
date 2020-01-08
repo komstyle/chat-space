510cc77d6784aaabffa937d7fd7c648d867c9c1a
komstyle <59528384+komstyle@users.noreply.github.com>
2020-01-08 17:16:10 +0900

    devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+<<<<<<< Updated upstream
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+=======
+  devise_for :users
+  root "messages#index"
+>>>>>>> Stashed changes
 end
