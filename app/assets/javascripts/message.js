a13016ad1a5a1e61bd29837da0e400e48179a751
komstyle <59528384+komstyle@users.noreply.github.com>
2020-01-14 20:11:26 +0900

    Automatic-updating2

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,6 @@
 $(function(){ 
   var buildHTML = function(message) {
     if (message.body && message.image) {
-      //data-idが反映されるようにしている
       var html = `<div class="mainBox" data-message-id="${message.id}">
         <div class="mainBox__user">
           <div class="mainBox__user__name">
@@ -19,7 +18,6 @@ $(function(){
         </div>
       </div>`
     } else if (message.body) {
-      //同様に、data-idが反映されるようにしている
       var html = `<div class="mainBox" data-message-id="${message.id}">
         <div class="mainBox__user">
           <div class="mainBox__user__name">
@@ -36,7 +34,6 @@ $(function(){
         </div>
       </div>`
     } else if (message.image) {
-      //同様に、data-idが反映されるようにしている
       var html = `<div class="mainBox" data-message-id="${message.id}>
         <div class="mainBox__user">
           <div class="mainBox__user__name">
@@ -78,26 +75,19 @@ $(function(){
     })
   })
   var reloadMessages = function() {
-    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
     last_message_id = $('.mainBox:last').data("message-id");
     $.ajax({
-      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
       url: "api/messages",
-      //ルーティングで設定した通りhttpメソッドをgetに指定
       type: 'get',
       dataType: 'json',
-      //dataオプションでリクエストに値を含める
       data: {id: last_message_id}
     })
     .done(function(messages) {
       if (messages.length !== 0) {
-        //追加するHTMLの入れ物を作る
         var insertHTML = '';
-        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
         $.each(messages, function(i, message) {
           insertHTML += buildHTML(message)
         });
-        //メッセージが入ったHTMLに、入れ物ごと追加
         $('.mainBody').append(insertHTML);
         $('.mainBody').animate({ scrollTop: $('.mainBody')[0].scrollHeight});
         $("form")[0].reset();
